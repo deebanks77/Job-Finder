92f803240ba78dc33f9d50bc948699fa01c1dfea
Banky <mac@macs-MacBook-Pro.local>
2023-02-22 11:58:22 +0100

    Initial commit

---
+++ src/pages/ProtectedRoute.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function protectedRoute() {
+  return <div>protectedRoute</div>;
+}
+
+export default protectedRoute;
---
+++ src/pages/Register.js
@@ -0,0 +1,124 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import styled from "styled-components";
+import { toast } from "react-toastify";
+
+// redux toolkit and useNavigate later
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
+// if possible prefer local state
+// global state
+
+function Register() {
+  const [values, setValues] = useState(initialState);
+
+  // redux toolkit and useNavigate later
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+
+    if (!email || !password || (!isMember && !name)) {
+      toast.error("please fill out the fields");
+    }
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+
+        {/* name field */}
+        {!values.isMember && (
+          <FormRow
+            name="name"
+            type="text"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        {/* email field */}
+        <FormRow
+          name="email"
+          type="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        {/* password field */}
+        <FormRow
+          name="password"
+          type="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+
+        {/* toggle button */}
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--primary-500);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  }
+`;
+
+export default Register;
